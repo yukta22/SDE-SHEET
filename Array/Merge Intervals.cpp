393b13e4581eac4b38f922c1b76c91d61fa211d0
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-29 00:50:23 +0530

    Merge Intervals[medium]

---
+++ Array/Merge Intervals.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/merge-intervals/
+//T.C.:O(nlogn) + O(n)
+//S.C.:O(n)
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> mergeIntervals;
+        if(intervals.size() == 0)  return mergeIntervals;
+        
+        sort(intervals.begin() , intervals.end());
+        vector<int> temp = intervals[0];
+        
+        for(auto it : intervals){
+            if(it[0] <= temp[1]){
+                temp[1] = max(it[1] , temp[1]);
+            }
+            else{
+                mergeIntervals.push_back(temp);
+                temp = it; 
+            }
+        }
+        mergeIntervals.push_back(temp);
+        return mergeIntervals;
+    }
+};
