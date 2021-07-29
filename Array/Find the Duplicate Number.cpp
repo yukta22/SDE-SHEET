0d425470a0ceca906bb518874c33de728d5b6c86
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-07-29 14:47:27 +0530

    Find the Duplicate Number [medium]

--- Array/Find the Duplicate Number.cpp
+++ Array/Find the Duplicate Number.cpp
@@ -44,8 +44,7 @@ public:
     int findDuplicate(vector<int>& nums) {
         int slow = nums[0];
         int fast = nums[0];
-  
-        
+          
         do{
             slow = nums[slow]; 
             fast = nums[nums[fast]]; 
