6d75fa2dc1fbecda7b4ddea6f75db94989ac58f2
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-08-11 12:14:40 +0530

    Copy List with Random Pointer[medium]

--- 2 - pointer/Copy List with Random Pointer.cpp
+++ 2 - pointer/Copy List with Random Pointer.cpp
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/copy-list-with-random-pointer/
+
 // T.C. == O(N) 
 //S.C. == O(N)
 class Solution {
@@ -14,3 +16,50 @@ public:
         
     }
 };
+
+
+//T.C. == O(N) 
+//S.C. == O(1)
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return head;
+        
+        Node* temp = head;
+        
+        //create a copy node 
+        while(temp){
+            Node* newNode = new Node(temp->val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            temp = newNode->next;
+        }
+        
+        //assigning random pointers
+        temp = head;
+        while(temp){
+            if(temp->random){
+                temp->next->random = temp->random->next;
+            }
+            temp = temp->next->next;
+        }
+        
+        //restore the original linked list
+        Node* newHead = head->next;
+        Node* temp2 = head->next;
+        temp = head;
+        
+        while(temp && temp2){
+            Node* currN = temp2->next;
+            temp->next = currN;
+            if(currN){
+                temp2->next = currN->next;
+            }
+            temp = temp->next;
+            temp2 = temp2->next;
+        }
+        
+        return newHead;
+    }
+};
+
