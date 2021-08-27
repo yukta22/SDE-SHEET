1633db9af514f83c6a987859c6aee6464e663c7d
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-08-27 22:09:14 +0530

    N-Queens[Hard]

---
+++ Backtracking/N-Queens.cpp
@@ -0,0 +1,70 @@
+//https://leetcode.com/problems/n-queens/
+
+/////////////////////////////////////////////////////1st approach/////////////////////////////////////////////////////
+
+class Solution {
+public:
+    
+    bool isSafe1(int row, int col, vector<string> board, int n) {
+        // check upper diagonal
+        int duprow = row;
+        int dupcol = col; 
+        
+        while(row >= 0 && col >= 0) {
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+        
+        //check row
+        row = duprow;
+        col = dupcol;
+        
+        while(col>=0) {
+            if(board[row][col] == 'Q') return false;
+            col--; 
+        }
+    
+        // check lower diagonal
+        row = duprow;
+        col = dupcol; 
+        while(row<n && col>=0) {
+            if(board[row][col] == 'Q') return false;
+            row++;
+            col--; 
+        }
+        
+        return true; 
+    }
+    
+    void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n) {
+        if(col == n) {
+            ans.push_back(board); 
+            return; 
+        }
+    
+        for(int row = 0;row<n;row++) {
+            if(isSafe1(row, col, board, n)) {
+                board[row][col] = 'Q'; 
+                solve(col+1, board, ans, n); 
+                board[row][col] = '.'; 
+            }
+        }
+    }   
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        
+        string s(n,'.');
+        for(int i = 0 ; i < n ; i ++){
+            board[i] = s;
+        }
+        
+        solve(0,board,ans,n);
+        return ans;
+        
+    }
+};
+
+
