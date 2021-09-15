7bb546b47110cdfa3b1dfdca773cc5a2780d8e3e
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-09-15 21:38:05 +0530

    Update Next Greater Element II.cpp

--- Stack and Queue/Next Greater Element II.cpp
+++ Stack and Queue/Next Greater Element II.cpp
@@ -7,8 +7,8 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> ans;
-        
+        vector<int> ans(n, -1);
+       
         for(int i = 0 ; i < nums.size() ; i++){
             int j=i+1;
             if(j==nums.size())
