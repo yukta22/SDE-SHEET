656540c5d8add8a2d3517cf3c15956c4941527e6
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-09-15 21:27:41 +0530

    Next Greater Element II[medium]

---
+++ Stack and Queue/Next Greater Element II.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/next-greater-element-ii/
+
+///////////////////////////////////////////////////// Brute-Force /////////////////////////////////////////////////////
+// T.C. : O(N^2) 
+// S.C. : O(N)
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> ans;
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            int j=i+1;
+            if(j==nums.size())
+                    j=0;
+            while(j!=i){
+                if(nums[j]>nums[i]){
+                    ans.push_back(nums[j]);
+                    break;
+                }
+                j++;
+                if(j==nums.size())
+                    j=0;
+            }
+        
+        }
+            
+         return ans;   
+    }
+};
+
+///////////////////////////////////////////////////// Brute-Force /////////////////////////////////////////////////////
+// T.C. : O(N^2) 
+// S.C. : O(N)
