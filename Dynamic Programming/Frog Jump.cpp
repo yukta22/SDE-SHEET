cc1bdcf64e36822976dab5fa7331218952f50234
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-06-16 09:54:13 +0530

    LeetCode

---
+++ Dynamic Programming/Frog Jump.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/frog-jump/
+
+class Solution {
+public:
+    bool canCross(vector<int>& stones) {
+        unordered_map<int , unordered_set<int>> hashMap;    
+        hashMap[stones[0] + 1] = {1};       
+        for(int i = 1 ; i < stones.size() ; ++i){       
+            int position = stones[i];                   
+            for(auto it : hashMap[position]){           
+                hashMap[position + it].insert(it);      
+                hashMap[position + it + 1].insert(it + 1);
+                hashMap[position + it - 1].insert(it - 1);
+            }
+        }
+        return hashMap[stones.back()].size() != 0;    
+    }
+};
