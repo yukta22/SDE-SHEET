f666e31a8ac88ed9669a4e39f746da1391546ece
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-06-23 21:47:45 +0530

    LeetCode

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int count = 0;
+        for(int i = 1 ; i < n ; i++){
+            count += max(0,prices[i] - prices[i-1]);
+        }
+       return count;
+
+    }
+};
