784e5a2fe0e71cff77f80b288a356726dfed8db0
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-06-24 21:41:42 +0530

    LeetCode

---
+++ Dynamic Programming/Largest Divisible Subset.cpp
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/largest-divisible-subset/
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> ans;
+        vector<int> dp(n+1,1);
+        
+        sort(nums.begin() , nums.end());
+        
+        if(n == 0) return ans;
+        int maxi = 1;
+        
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(nums[i] % nums[j] == 0){
+                    dp[i] = max(dp[i],dp[j]+1);
+                    maxi = max(maxi,dp[i]);
+                }
+            }
+        }
+        
+        int prev = -1;
+        for(int i = n-1 ; i >= 0 ; i--){
+            if(dp[i] == maxi && (prev % nums[i] == 0 || prev == -1)){
+                ans.push_back(nums[i]);
+                maxi -= 1;
+                prev = nums[i];
+            }
+        }
+        
+        return ans;
+    }
+};
