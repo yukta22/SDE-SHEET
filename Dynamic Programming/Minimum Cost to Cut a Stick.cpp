8e85d889309b6ecd944d9e57d9cdd500cd5ea031
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-06-28 21:43:14 +0530

    LeetCode

---
+++ Dynamic Programming/Minimum Cost to Cut a Stick.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+
+class Solution {
+public:
+    
+     int f(int n, int c,vector<int>& cuts,int i,int j, vector<vector< int>>&dp){
+        if(i>j)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int mini=1e7;
+        
+    
+        for(int k=i;k<=j;k++)
+        {
+            int steps= cuts[j+1] - cuts[i-1] + f(n,c,cuts,i,k-1,dp) + f(n,c,cuts,k+1,j,dp);
+            mini=min((int)steps,mini);
+        }
+        return dp[i][j]=mini;
+            
+    }
+    
+    int minCost(int n, vector<int>& cuts) {
+        int c=cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+                
+        sort(cuts.begin(),cuts.end());
+        vector<vector< int>>dp(c+1,vector<int>(c+1,-1));
+        return f(n,c,cuts,1,c,dp);
+
+    }
+};
