837d9a613b259065808e11a36cfd7364f139196c
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-07-01 13:50:47 +0530

    LeetCode

---
+++ Binary Tree/Same Tree.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/same-tree/
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL || q == NULL){
+            return (p==q);
+        }
+        
+        return (p->val == q->val) && isSameTree(p->left , q->left) 
+                                  && isSameTree(p->right , q->right);
+    }
+};
+
