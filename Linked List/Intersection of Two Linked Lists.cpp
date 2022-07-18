49a420468070739ca2553d9b7a7b471037129306
yukta22 <60733741+yukta22@users.noreply.github.com>
2022-07-18 08:15:11 +0530

    LeetCode

---
+++ Linked List/Intersection of Two Linked Lists.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* dummy1 = headA;
+        ListNode* dummy2 = headB;
+        
+        while(dummy1 != dummy2){
+            dummy1 = dummy1 == NULL ? headB : dummy1->next;
+            dummy2 = dummy2 == NULL ? headA : dummy2->next;
+        }
+        return dummy1;
+    }
+};
